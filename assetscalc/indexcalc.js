3a8acc4e4d455f5cb2169356bf55e33af32a6f0b
Tania Novikova <tanianovikova74@gmail.com>
2022-09-03 09:24:00 +0300

    homework 13

---
+++ assetscalc/indexcalc.js
@@ -0,0 +1,25 @@
+let first = prompt("Введите первое число");
+let second = prompt("Введите вторе число");
+alert();
+
+function sum(num1,num2){
+    return  Number("num1") + Number("num2");
+}
+alert(sum)
+
+function min(num1,num2){
+    return  num1 - num2;
+}
+alert(min)
+
+function multiply(num1,num2){
+    return  num1 * num2;
+}
+alert(multiply)
+
+function divide(num1,num2){
+    return  num1 / num2;
+}
+alert(divide)
+
+
